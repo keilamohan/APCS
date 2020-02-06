2b023961e1eefa1a5722be3c3a76e35c04905d6c
keilamohan <60747314+keilamohan@users.noreply.github.com>
2020-02-06 08:27:36 -0800

    hehehhe

---
+++ chapter 6/src/labs/test.java
@@ -0,0 +1,12 @@
+package labs;
+import java.util.Scanner;
+
+public class test
+{
+    public static void main(String[]args)
+{
+    System.out.println("How much?");
+    Scanner scan = new Scanner(System.in);
+    scan.nextInt();
+}
+}
