819b0dc2009fc7d4514b2fd1cc73c953c0829af6
keilamohan <60747314+keilamohan@users.noreply.github.com>
2020-02-10 19:08:06 -0800

    hehheeheheh

--- chapter 6/src/labs/Sales.java
+++ chapter 6/src/labs/Sales.java
@@ -7,19 +7,18 @@ package labs;
 //
 // ****************************************************************
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Sales
 {
-    public static void main(String[] args)
+    public static void main(final String[] args)
     {
-	Scanner scan = new Scanner(System.in);
+	final Scanner scan = new Scanner(System.in);
 
 
 	System.out.println("Enter in an amount of sales people: ");
-	int salespeople = scan.nextInt();
+	final int salespeople = scan.nextInt();
 	
-	int[] sales = new int[salespeople];
+	final int[] sales = new int[salespeople];
 	int sum;
 	int maxSale = 0;
 	int maxId = 0;
@@ -30,17 +29,17 @@ public class Sales
 	for (int i=0; i<sales.length; i++)
 	    {
 
-			System.out.print("Enter sales for salesperson " + i+1 + ": ");
+			System.out.print("Enter sales for salesperson " + (i+1) + ": ");
 			sales[i] = scan.nextInt();
 			if (sales[i] > maxSale)
 			{
 				maxSale = sales[i];
-				maxId = i+1;
+				maxId = (i+1);
 			}
 			if (sales[i] < minSale)
 			{
 				minSale = sales[i];
-				minId = i+1;
+				minId = (i+1);
 			}
 	    }
 
@@ -54,23 +53,24 @@ public class Sales
 	    }
 
 	
-	int average = sum/SALESPEOPLE;
+	final int average = sum/salespeople;
 
 
 	System.out.println("\nTotal sales: " + sum);
 	
 	System.out.println("Average sales: " + average);
 
-	System.out.println("Salesperson " + maxId + "had the highest sale with " + maxSale);
+	System.out.println("Salesperson " + maxId + " had the highest sale with " + maxSale);
 	
-	System.out.println("Salesperson " + minId + "had the lowest sale with " + minSale);
+	System.out.println("Salesperson " + minId + " had the lowest sale with " + minSale);
 	
 	System.out.println("Please print a value: ");
-	int val = scan.nextInt();
+	final int val = scan.nextInt();
 
 
-	int exceed[];
-	int exceedId;
+
+	int[] exceed = new int[salespeople];
+
 	for (int i =0; i< sales.length; i++)
 	{
 		if (sales[i] > val)
@@ -83,7 +83,11 @@ public class Sales
 
 	for (int i= 0; i < exceed.length; i++)
 	{
-		System.out.println("Salesperson " + i+1 + " exceeded the value entered with a sale of " + exceed[i]);
+		System.out.println("Salesperson " + (i+1) + " exceeded the value entered with a sale of " + exceed[i]);
 	}
-    System.out.println("The total number of people who exceeded the value entered is " + exceed.length);
+	System.out.println("The total number of people who exceeded the value entered is " + exceed.length);
+
+	scan.close();
 }
+}
+	
