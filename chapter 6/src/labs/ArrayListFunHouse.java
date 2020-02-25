929bc216e14ef363624ec26e583170792a8011b7
keilamohan <60747314+keilamohan@users.noreply.github.com>
2020-02-25 12:42:29 -0800

    yayaya

---
+++ chapter 6/src/labs/ArrayListFunHouse.java
@@ -0,0 +1,42 @@
+package labs;
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import static java.lang.System.*;
+
+public class ArrayListFunHouse
+{
+	public static ArrayList<Integer> getListOfFactors(int number)
+	{
+        ArrayList<Integer> factors = new ArrayList<Integer>();
+        
+        for(int i = 1; i <= Math.sqrt(number); i++)
+        {
+            if(number % i == 0 && i > 1)
+            {
+				factors.add(i);
+                if (i != number/i)
+                {
+					factors.add(number/i);
+				}
+			}
+		}
+        Collections.sort(factors);
+        
+		return factors;
+	}
+	
+	public static void keepOnlyCompositeNumbers( List<Integer> nums )
+	{
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (getListOfFactors(nums.get(i)).size() == 0)
+            {
+				nums.remove(i);
+				i--;
+			}
+		}
+	}
+}
