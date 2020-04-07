e2448831391d0ebe8b467f7d88c75d57001bd4dd
keilamohan <60747314+keilamohan@users.noreply.github.com>
2020-04-07 05:36:35 -0700

    oh

--- chapter magpie/activity5/Magpie5.java
+++ chapter magpie/activity5/Magpie5.java
@@ -284,7 +284,11 @@ public class Magpie5
 	private String [] randomResponses = {"Interesting, tell me more",
 			"Hmmm.",
 			"Do you really think so?",
-			"You don't say."
+			"You don't say.",
+			"Wow. Cool.",
+			"Intriguing, please say more about this.",
+			"Nice!",
+			"Love to hear that."
 	};
 	
 }
